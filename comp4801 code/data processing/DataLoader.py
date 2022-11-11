fa9e0bcd88a586a6026fd82e3e8de319967ee628
ThomasPoonxdd <s243927@gmail.com>
2022-11-11 18:35:22 +0800

    change dataloader

--- comp4801 code/data processing/DataLoader.py
+++ comp4801 code/data processing/DataLoader.py
@@ -13,25 +13,50 @@ class LvisDataset(Dataset):
                 file_contents = user_file.read()
             parsed_json = json.loads(file_contents)
             
+            # print(parsed_json.keys())
+            # dict_keys(['info', 'categories', 'annotations', 'images', 'licenses'])
+            # separate them into three different list
             images = parsed_json['images']
             annotations = parsed_json['annotations']
-            print(len(images),len(annotations))
-            dic = {}
-            for i in range(len(annotations)):
-                if annotations[i]['image_id'] not in dic.keys():
-                    dic[annotations[i]['image_id']] = [annotations[i]]
+            categories = parsed_json['categories']
+            # print(len(images),len(annotations),len(categories))
+            new_images = []
+            new_annotations = []
+            new_categories = []
+            images_dic = {}
+            categories_dic = {}
+            
+        
+            for i in range(len(images)):
+                if images[i]['id'] not in images_dic.keys():
+                    images_dic[images[i]['id']] = [images[i]]
                 else:
-                    dic[annotations[i]['image_id']].append(annotations[i])
-            print(len(dic),dic)
+                    images_dic[images[i]['id']].append(images[i])
+                    
+            for i in range(len(categories)):
+                if categories[i]['id'] not in images_dic.keys():
+                    categories_dic[categories[i]['id']] = [categories[i]]
+                else:
+                    categories_dic[categories[i]['id']].append(categories[i])      
+            
+            for i in range(len(annotations)):
+                new_images.append(annotations[i])
+                new_annotations.append(images_dic[annotations[i]['image_id']][0])
+                new_categories.append(categories_dic[annotations[i]['category_id']][0])
+            
+            self.images = new_images
+            self.annotations = new_annotations
+            self.categories =  new_categories
+            self.n_samples = len(new_images)  
             
                 
 
     
-    # def __getitem__(self, index):
-    #     return self.x[index] , self.y[index]
+    def __getitem__(self, index):
+        return self.images[index] , self.annotations[index], self.categories[index]
 
-    # def __len__(self):
-    #     return self.n_samples
+    def __len__(self):
+        return self.n_samples
     
 if __name__ == '__main__':
     a = LvisDataset(json_path="C:/Users/User/Desktop/lvis_v1_val.json")
