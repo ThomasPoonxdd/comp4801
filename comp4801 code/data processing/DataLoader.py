959351b656d434589ad294e4ba8aa6f0598f9954
ThomasPoonxdd <s243927@gmail.com>
2022-11-11 20:45:44 +0800

    change data loader

--- comp4801 code/data processing/DataLoader.py
+++ comp4801 code/data processing/DataLoader.py
@@ -1,9 +1,11 @@
 import torch
+import torchvision
 from torch.utils.data import Dataset
 import numpy as np
 import pandas as pd
 import os
 import json
+from PIL import Image
 
 
 class LvisDataset(Dataset):
@@ -20,43 +22,62 @@ class LvisDataset(Dataset):
             annotations = parsed_json['annotations']
             categories = parsed_json['categories']
             # print(len(images),len(annotations),len(categories))
-            new_images = []
-            new_annotations = []
-            new_categories = []
-            images_dic = {}
-            categories_dic = {}
+            # new_images = []
+            # new_annotations = []
+            # new_categories = []
+            images_d = {}
+            bbox_d = {}
             
         
             for i in range(len(images)):
-                if images[i]['id'] not in images_dic.keys():
-                    images_dic[images[i]['id']] = [images[i]]
+                if images[i]['id'] not in images_d.keys():
+                    image_name = images[i]['coco_url'].split('/')[-1]
+                    path_prefix ="./train/"
+                    image_path = os.path.join(path_prefix, image_name)
+                    image = Image.open(image_path)
+                    to_tensor = torchvision.transforms.ToTensor()
+                    image = to_tensor(image)
+                    images_d[images[i]['id']] = [image]
+                    break
                 else:
-                    images_dic[images[i]['id']].append(images[i])
-                    
-            for i in range(len(categories)):
-                if categories[i]['id'] not in images_dic.keys():
-                    categories_dic[categories[i]['id']] = [categories[i]]
-                else:
-                    categories_dic[categories[i]['id']].append(categories[i])      
+                    # images_dic[images[i]['id']].append(images[i])
+                    print("Error: duplicate image id")
+            
+            # for i in range(len(categories)):
+            #     if categories[i]['id'] not in categories_dic.keys():
+            #         categories_dic[categories[i]['id']] = [categories[i]]
+            #     else:
+            #         categories_dic[categories[i]['id']].append(categories[i])      
             
             for i in range(len(annotations)):
-                new_images.append(annotations[i])
-                new_annotations.append(images_dic[annotations[i]['image_id']][0])
-                new_categories.append(categories_dic[annotations[i]['category_id']][0])
+                img_id = annotations[i]['image_id']
+                cat_id = annotations[i]['category_id']
+                one_hot = torch.zeros(len(categories))
+                one_hot[cat_id] = 1
+                
+                if img_id not in bbox_d.keys():
+                    bbox_d[img_id] = one_hot
+                else:
+                    bbox_d[img_id] = torch.stack((bbox_d[img_id], one_hot), dim = 0)
+                break
+                # new_images.append(annotations[i])
+                # new_annotations.append(images_dic[annotations[i]['image_id']][0])
+                # new_categories.append(categories_dic[annotations[i]['category_id']][0])
             
-            self.images = new_images
-            self.annotations = new_annotations
-            self.categories =  new_categories
-            self.n_samples = len(new_images)  
+            self.images = images_d
+            self.bbox = bbox_d
+            # self.annotations = new_annotations
+            # self.categories =  new_categories
+            # self.n_samples = len(new_images)  
             
                 
 
     
     def __getitem__(self, index):
-        return self.images[index] , self.annotations[index], self.categories[index]
+        return self.images[index] , self.bbox[index]
 
     def __len__(self):
-        return self.n_samples
+        return len(self.images)
     
 if __name__ == '__main__':
     a = LvisDataset(json_path="C:/Users/User/Desktop/lvis_v1_val.json")
