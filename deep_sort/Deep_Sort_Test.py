a351209a92eb78e45c975895f9e9019cffc37106
kenchanLOL <chankwankin3@gmail.com>
2023-03-12 17:34:28 +0800

    rename folders and files

---
+++ deep_sort/Deep_Sort_Test.py
@@ -0,0 +1,16 @@
+from deep_sort_realtime.deepsort_tracker import DeepSort
+
+def Deep_Sort_Test(frame=None):
+    
+    deepSortTracker = DeepSort(max_age=10)
+    bbs = object_detector.detect(frame) 
+    tracks = deepSortTracker.update_tracks(bbs, frame=frame)
+    for track in tracks:
+        if not track.is_confirmed():
+            continue
+        track_id = track.track_id
+        ltrb = track.to_ltrb()
+    
+
+if __name__ == '__main__':
+    Deep_Sort_Implement()
