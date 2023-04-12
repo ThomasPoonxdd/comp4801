89a769bc32d6b6f40f901c4fcf3ed3e3f078272a
unknown <s243927@gmail.com>
2023-04-12 23:35:54 +0800

    add camera calibration

---
+++ camera calibration/getImages.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture(0)
+
+num = 0
+script_dir = os.path.dirname(__file__)
+
+while cap.isOpened():
+
+    succes, img = cap.read()
+
+    k = cv2.waitKey(5)
+
+    if k == 27:
+        break
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite(f'{script_dir}/images/img' + str(num) + '.png', img)
+        print("image saved!")
+        num += 1
+
+    cv2.imshow('Img',img)
+
+# Release and destroy all windows before termination
+cap.release()
+
+cv2.destroyAllWindows()
